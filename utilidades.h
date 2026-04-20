a9bc068c4b650e0da148b38024f19d6fc7296043
Camilovillanuevag <165525994+Camilovillanuevag@users.noreply.github.com>
2026-04-20 00:15:11 -0500

    Implementación de la clase equipo y mejora de la clase utilidades

--- utilidades.h
+++ utilidades.h
@@ -1,8 +1,13 @@
 #ifndef UTILIDADES_H
 #define UTILIDADES_H
-
 char* copiarCadena(const char* origen);
 int longitudCadena(const char* cad);
-
-
+bool sonIguales(const char* a, const char* b);
+void copiarCadenaEnBuffer(char* destino, const char* origen);
+int stringAEntero(const char* cad);
+void enteroACadena(int n, char* buffer);
+void inicializarAleatorio();
+int aleatorioEnRango(int minimo, int maximo);
+void intercambiarEnteros(int& a, int& b);
+void intercambiarPunteros(void*& a, void*& b);
 #endif
